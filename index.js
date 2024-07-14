ab842d142aed94845e16e04f573a62b3c77e1f9e
Tony <kipkoechtony510@gmail.com>
2024-07-14 19:21:56 +0300

    Complete employee object operations assignment

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution in this file!
+// employee.js
+
+// index.js
+
+// index.js
+
+const employee = {
+    name: "Mark",
+    streetAddress: "Kimberly Street 42"
+  };
+  
+  function updateEmployeeWithKeyAndValue(emp, key, value) {
+    return {
+      ...emp,
+      [key]: value
+    };
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(emp, key, value) {
+    emp[key] = value;
+    return emp;
+  }
+  
+  function deleteFromEmployeeByKey(emp, key) {
+    const newEmployee = { ...emp };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(emp, key) {
+    delete emp[key];
+    return emp;
+  }
+  
+  // Export the employee and functions for testing
+  module.exports = {
+    employee,
+    updateEmployeeWithKeyAndValue,
+    destructivelyUpdateEmployeeWithKeyAndValue,
+    deleteFromEmployeeByKey,
+    destructivelyDeleteFromEmployeeByKey
+  };
+  
+  
+  
+
